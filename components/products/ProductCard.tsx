1c453a7f725a3c6c591e521ea181d4f95d235037
emmarp <ruiizemmanuel@gmail.com>
2024-06-06 06:57:52 -0600

    Resaltando la categoría actual

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -17,20 +17,18 @@ export default function ProductCard({ product }: ProductCard) {
       />
 
       <div className="p-5">
-        <h3 className="text-2xl font-bold">
-          <h3 className="text-2xl font-bold">{product.name}</h3>
+        <h3 className="text-2xl font-bold">{product.name}</h3>
 
-          <p className="mt-5 font-black text-4xl text-amber-500">
-            {formatCurrency(product.price)}
-          </p>
+        <p className="mt-5 font-black text-4xl text-amber-500">
+          {formatCurrency(product.price)}
+        </p>
 
-          <button
-            type="button"
-            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
-          >
-            Agregar
-          </button>
-        </h3>
+        <button
+          type="button"
+          className="bg-indigo-500 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+        >
+          Agregar
+        </button>
       </div>
     </div>
   );
