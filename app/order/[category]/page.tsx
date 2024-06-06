a16ff03f522e43fdbcf1ea46ad190982692f1b0c
emmarp <ruiizemmanuel@gmail.com>
2024-06-06 06:46:57 -0600

    Imagen y botón 'Agregar' añadidos al ProductCard

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -18,6 +18,10 @@ export default async function OrderPage({
 
   return (
     <>
+      <h1 className="text-2xl my-10">
+        Elige y personaliza tu pedido a continuación
+      </h1>
+
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { formatCurrency } from "@/src/utils";
 import { Product } from "@prisma/client";
+import Image from "next/image";
 
 type ProductCard = {
   product: Product;
@@ -8,12 +9,27 @@ type ProductCard = {
 export default function ProductCard({ product }: ProductCard) {
   return (
     <div className="border bg-white">
+      <Image
+        src={`/products/${product.image}.jpg`}
+        alt={product.name}
+        width={500}
+        height={500}
+      />
+
       <div className="p-5">
         <h3 className="text-2xl font-bold">
           <h3 className="text-2xl font-bold">{product.name}</h3>
+
           <p className="mt-5 font-black text-4xl text-amber-500">
             {formatCurrency(product.price)}
           </p>
+
+          <button
+            type="button"
+            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+          >
+            Agregar
+          </button>
         </h3>
       </div>
     </div>
