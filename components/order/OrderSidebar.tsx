6b0086ab1ecc54cabe7b203c9ada12e9d27117cb
emmarp <ruiizemmanuel@gmail.com>
2024-06-06 05:29:35 -0600

    Conexión global a prisma

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { prisma } from "@/src/prisma";
 
-export default function OrderSidebar() {
-  return <aside className="md:w-72 md:h-screen bg-white"></aside>;
+async function getCategories() {
+  return await prisma.category.findMany();
+}
+
+export default async function OrderSidebar() {
+  const categories = await getCategories();
+
+  console.log(categories);
+
+  return <aside className="md:w-72 md:h-screen bg-white">OrderSidebar</aside>;
 }
