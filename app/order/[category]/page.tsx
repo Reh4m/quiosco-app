60c23d59d3032b00f146440e0e1024733dbb2831
emmarp <ruiizemmanuel@gmail.com>
2024-06-06 06:39:05 -0600

    Mostrando lista de producto para cada categoría

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,3 +1,4 @@
+import ProductCard from "@/components/products/ProductCard";
 import { prisma } from "@/src/prisma";
 
 async function getProducts(category: string) {
@@ -15,5 +16,13 @@ export default async function OrderPage({
 }) {
   const products = await getProducts(params.category);
 
-  return <div>OrderPage</div>;
+  return (
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
 }
