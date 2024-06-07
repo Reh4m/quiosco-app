1746acc0867699d41a83d5ef4a3208cc19d80def
emmarp <ruiizemmanuel@gmail.com>
2024-06-07 05:49:33 -0600

    Actualizar lista de ordenes al completar

--- actions/complete-order-action.ts
+++ actions/complete-order-action.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { prisma } from "@/src/prisma";
 import { OrderIdSchema } from "@/src/schema";
 
@@ -21,6 +22,8 @@ export async function coompleteOrder(formData: FormData) {
           orderReadyAt: new Date(Date.now()),
         },
       });
+
+      revalidatePath("/admin/orders");
     } catch (error) {
       console.error(error);
     }
