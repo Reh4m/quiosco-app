30f475ca7c08966952bfbcfa21a666755652f48a
emmarp <ruiizemmanuel@gmail.com>
2024-06-07 06:35:47 -0600

    Validación de paginación en la URL

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,6 +2,7 @@ import ProductsPagination from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/prisma";
+import { redirect } from "next/navigation";
 
 async function productCount() {
   return await prisma.product.count();
@@ -31,6 +32,8 @@ export default async function ProductsPage({
   const page = +searchParams.page || 1;
   const pageSize = 10;
 
+  if (page < 0) redirect("/admin/products");
+
   const productsData = getProducts(page, pageSize);
   const totalProductsData = productCount();
   const [products, totalProducts] = await Promise.all([
@@ -38,6 +41,8 @@ export default async function ProductsPage({
     totalProductsData,
   ]);
   const totalPages = Math.ceil(totalProducts / pageSize);
+
+  if (page > totalPages) redirect("/admin/products");
 
   return (
     <>
