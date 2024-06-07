2eaf3fc087fbfe02578ec1173c090788654a6b36
emmarp <ruiizemmanuel@gmail.com>
2024-06-07 05:53:35 -0600

    Fix: Ruta '/admin/orders' incorrecta

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,36 +1,9 @@
-import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/prisma";
 
-async function getPendingOrders() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false,
-    },
-    include: {
-      orderProducts: { include: { product: true } },
-    },
-  });
-
-  return orders;
-}
-
-export default async function ProductsPage() {
-  const orders = await getPendingOrders();
-
+export default function ProductsPage() {
   return (
     <>
       <Heading>Administrar productos</Heading>
-
-      {orders.length ? (
-        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map((order) => (
-            <OrderCard key={order.id} order={order} />
-          ))}
-        </div>
-      ) : (
-        <p className="text-center">No hay ordenes pendientes</p>
-      )}
     </>
   );
 }
