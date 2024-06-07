558e7bab9cd710eca7fcb889d6c4a964a09bee47
emmarp <ruiizemmanuel@gmail.com>
2024-06-07 08:41:28 -0600

    Obtener el producto a editar

---
+++ app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,37 @@
+import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/prisma";
+import notFound from "./not-found";
+
+async function getProductById(id: number) {
+  const product = await prisma.product.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!product) {
+    notFound();
+  }
+
+  return product;
+}
+
+export default async function EditProductsPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const product = await getProductById(+params.id);
+
+  return (
+    <>
+      <Heading>Editar Producto: {product.name}</Heading>
+
+      {/* <GoBackButton />
+
+      <EditProductForm>
+        <ProductForm product={product} />
+      </EditProductForm> */}
+    </>
+  );
+}
