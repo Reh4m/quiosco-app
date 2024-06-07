2f7462e48899729ac5c5d3ef4dd8d5f02b5b3a62
emmarp <ruiizemmanuel@gmail.com>
2024-06-07 05:14:19 -0600

    Obtener ordenes pendientes

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,6 +1,22 @@
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/prisma";
 
-export default function ProductsPage() {
+async function getPendingOrders() {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false,
+    },
+    include: {
+      orderProducts: { include: { product: true } },
+    },
+  });
+
+  return orders;
+}
+
+export default async function ProductsPage() {
+  const orders = await getPendingOrders();
+
   return (
     <>
       <Heading>Administrar productos</Heading>
