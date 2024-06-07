08ef0beea605d7ca4ca1524f85a4284d9c1d9709
emmarp <ruiizemmanuel@gmail.com>
2024-06-07 07:09:31 -0600

    Mostrar resultados de la búsqueda

---
+++ app/admin/products/search/page.tsx
@@ -0,0 +1,44 @@
+import ProductsSearchForm from "@/components/products/ProductsSearchForm";
+import ProductsTable from "@/components/products/ProductsTable";
+import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/prisma";
+
+async function searchProducts(searchTerm: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      name: {
+        contains: searchTerm,
+        mode: "insensitive",
+      },
+    },
+    include: {
+      category: true,
+    },
+  });
+
+  return products;
+}
+
+export default async function SearchPage({
+  searchParams,
+}: {
+  searchParams: { search: string };
+}) {
+  const products = await searchProducts(searchParams.search);
+
+  return (
+    <>
+      <Heading>Resultados de búsqueda: {searchParams.search}</Heading>
+
+      <div className="flex flex-col lg:flex-row lg:justify-end gap-5">
+        <ProductsSearchForm />
+      </div>
+
+      {products.length ? (
+        <ProductsTable products={products} />
+      ) : (
+        <p className="text-center text-lg">No hay resultados</p>
+      )}
+    </>
+  );
+}
